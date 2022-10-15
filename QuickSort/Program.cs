9fcec9eaef7f67ab46d3e729ec7e35167b7f48e6
Breno Robert <brenorobert@ucl.br>
2022-10-15 18:58:56 -0300

    Testes

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -9,7 +9,12 @@ namespace QuickSort
         {
 
             // int[] values = { 3, 8, 7, 10, 0, 23, 2, 1, 77, 7 };
-            List<String> palavras = new() { "Xuxu", "Xuxu", "Cenoura", "Picles", "Repolho", "Brocolis", "Couve-flor", "Alface"};
+            List<String> palavras = new() {
+                "abac",
+                "aaac",
+                "aaa",
+                "aab"
+            };// new() { "Xuxu", "Xuxu", "Cenoura", "Picles", "Repolho", "Brocolis", "Couve-flor", "Alface"};
             QuickSortMethod.QuickSort(ref palavras);
             String printado = String.Join(", ", palavras);
             Console.WriteLine(printado);
